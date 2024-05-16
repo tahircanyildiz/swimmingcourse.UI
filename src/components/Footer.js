59b10723b3fb5e87b2bad574004a5f41096700be
tahircanyildiz <tahircanyildiz@hotmail.com>
2024-05-16 17:02:51 +0300

    Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <p>© 2024 MyWebsite. All Rights Reserved.</p>
-      </div>
+        <div className="footer-content">
+          <div className="social-links">
+            <a href="https://twitter.com/Tahiir307" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+            <a href="https://www.facebook.com/tahircany.99" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+            <a href="https://www.instagram.com/mrpurposeless/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          </div>
+          <p className="follow-text">Follow us on social media</p>
+          <p className="rights-text">© 2024 MyWebsite. All Rights Reserved.</p>
+        </div>
+      </div>    
     </footer>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,7 @@ function Home() {
       <AboutPage />
       <Coach/>
       <Contact />
+      <Footer/>
        
     </div>
   )
